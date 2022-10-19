cd81e2c66ceb4df9337271d2317016ba5f61f4d0
Seo Choi <seochoi56@gmail.com>
2022-10-19 12:57:14 -0400

    commit tree c302dfc7b157fa9ded60cc20a99e47fd65fc7139

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 class Triangle
   # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def validTriangle 
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2 || @side1 <= 0 || @side2 <=0 || @side3 <=0 
+      return false
+    else 
+      return true
+    end
+  end
+
+  def kind
+    if !validTriangle
+      begin 
+        raise TriangleError
+      end
+    else
+      if @side1 == @side2 && @side1 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "This is not a valid triangle!"
+    end
+  end
+
 end
